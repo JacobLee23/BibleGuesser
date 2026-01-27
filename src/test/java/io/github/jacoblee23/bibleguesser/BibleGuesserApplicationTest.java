b220828616a82735ed978daa04cd7620f4fdbd6d
Jacob Lee <Jacob.J.Lee@outlook.com>
2026-01-27 14:22:01 -0800

    Create Maven project

---
+++ src/test/java/io/github/jacoblee23/bibleguesser/BibleGuesserApplicationTest.java
@@ -0,0 +1,12 @@
+package io.github.jacoblee23.bibleguesser;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+
+class BibleGuesserApplicationTest {
+    @Test
+    void sanityTest() {
+        assertTrue(true);
+    }
+}
