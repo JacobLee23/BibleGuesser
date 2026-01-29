081121e1988e50419cdb5f1b02f1caa11c26fd3e
Jacob Lee <Jacob.J.Lee@outlook.com>
2026-01-28 18:46:53 -0800

    Create class to parse testament/genre (sub)divisions of Biblical canon

---
+++ src/main/java/io/github/jacoblee23/bibleguesser/canon/Canon.java
@@ -0,0 +1,121 @@
+package io.github.jacoblee23.bibleguesser.canon;
+
+import java.io.InputStream;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.yaml.snakeyaml.Yaml;
+
+
+/**
+ * Interfaces for parsing the testament divisions and genre subdivisions of the Biblical canon.
+ */
+public class Canon {
+    private static Canon instance = null;
+    private static final String PATH = "/canon.yml";
+
+    private final Map<String, Map<String, List<String>>> data;
+
+    private Canon() {
+        InputStream in = getClass().getResourceAsStream(Canon.PATH);
+        Yaml yaml = new Yaml();
+
+        this.data = new LinkedHashMap<>(yaml.load(in));
+    }
+
+    @Override
+    public String toString() { return this.data.toString(); }
+
+    /**
+     * Global access point to the class singleton.
+     *
+     * @return The single instance of the class
+     */
+    public static Canon getInstance() {
+        if (Canon.instance == null) {
+            Canon.instance = new Canon();
+        }
+        return Canon.instance;
+    }
+
+    /**
+     * Retrieves the list of testament divisions that comprise the Biblical canon.
+     *
+     * @return The testament divisions of the Biblical canon
+     */
+    public List<String> getTestaments() {
+        List<String> testaments = new LinkedList<>();
+        this.data.forEach((key, value) -> { testaments.add(key); });
+
+        return testaments;
+    }
+
+    /**
+     * Retrieves the list of genre subdivisions that comprise a specified testament division of the
+     * Biblical canon.
+     *
+     * @param testament A testament division
+     * @return The genre subdivisions that comprise the specified testament
+     */
+    public List<String> getGenres(String testament) {
+        List<String> genres = new LinkedList<>();
+        this.data.get(testament).forEach((key, value) -> { genres.add(key); });
+
+        return genres;
+    }
+
+    /**
+     * Retrieves the list of genre subdivisions that comprise the Biblical canon.
+     *
+     * @return The genre subdivisions that comprise the Biblical canon
+     */
+    public List<String> getGenres() {
+        List<String> genres = new LinkedList<>();
+        for (String testament : this.getTestaments()) {
+            genres.addAll(this.getGenres(testament));
+        }
+        return genres;
+    }
+
+    /**
+     * Retrieves the list of books that comprise a specified genre subdivision of a specified
+     * testament division of the Biblical canon.
+     *
+     * @param testament A testament division
+     * @param genre A genre subdivision
+     * @return The books that comprise the specified genre of the specified testament
+     */
+    public List<String> getBooks(String testament, String genre) {
+        return this.data.get(testament).get(genre);
+    }
+
+    /**
+     * Retrieves the list of books that comprise a specified testament division of the Biblical
+     * canon.
+     *
+     * @param testament A testament division
+     * @return The books that comprise the specified testament
+     */
+    public List<String> getBooks(String testament) {
+        List<String> books = new LinkedList<>();
+        for (String genre : this.getGenres(testament)) {
+            books.addAll(this.getBooks(testament, genre));
+        }
+        return books;
+    }
+
+    /**
+     * Retrieves the list of books that comprise the Biblical canon.
+     *
+     * @return The books that comprise the Biblical canon
+     */
+    public List<String> getBooks() {
+        List<String> books = new LinkedList<>();
+        for (String testament : this.getTestaments()) {
+            books.addAll(this.getBooks(testament));
+        }
+        return books;
+    }
+}
---
+++ src/test/java/io/github/jacoblee23/bibleguesser/canon/CanonTest.java
@@ -0,0 +1,86 @@
+package io.github.jacoblee23.bibleguesser.canon;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+
+public class CanonTest {
+    @Test
+    void testGetTestaments() {
+        Canon canon = Canon.getInstance();
+
+        List<String> testaments = Arrays.asList("Old Testament", "New Testament");        
+        assertEquals(testaments, canon.getTestaments());
+    }
+
+    @Test
+    void testGetGenres() {
+        Canon canon = Canon.getInstance();
+
+        List<String> genres = Arrays.asList(
+            // Old Testament
+            "Law",
+            "Historical Narrative",
+            "Wisdom and Poetry",
+            "Major Prophets",
+            "Minor Prophets",
+
+            // New Testament
+            "Gospel Accounts",
+            "Early Church History",
+            "Pauline Epistles",
+            "General Epistles",
+            "Apocalyptic Literature"
+        );
+        assertEquals(genres, canon.getGenres());
+    }
+
+    @Test
+    void testGetBooks() {
+        Canon canon = Canon.getInstance();
+
+        List<String> books = Arrays.asList(
+            /* Old Testament */
+
+            // Law
+            "Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy",
+
+            // Historical Narrative
+            "Joshua", "Judges", "Ruth", "1 Samuel", "2 Samuel", "1 Kings", "2 Kings",
+            "1 Chronicles", "2 Chronicles", "Ezra", "Nehemiah", "Esther",
+
+            // Wisdom and Prophecy
+            "Job", "Psalms", "Proverbs", "Ecclesiastes", "Song of Solomon",
+
+            // Major Prophets
+            "Isaiah", "Jeremiah", "Lamentations", "Ezekiel", "Daniel",
+
+            // Minor Prophets
+            "Hosea", "Joel", "Amos", "Obadiah", "Jonah", "Micah", "Nahum", "Habakkuk", "Zephaniah",
+            "Haggai", "Zechariah", "Malachi",
+
+            /* New Tesetament */
+
+            // Gospel Accounts
+            "Matthew", "Mark", "Luke", "John",
+
+            // Early Church History
+            "Acts",
+
+            // Pauline Epistles
+            "Romans", "1 Corinthians", "2 Corinthians", "Galatians", "Ephesians", "Philippians",
+            "Colossians", "1 Thessalonians", "2 Thessalonians", "1 Timothy", "2 Timothy", "Titus",
+            "Philemon",
+
+            // General Epistles
+            "Hebrews", "James", "1 Peter", "2 Peter", "1 John", "2 John", "3 John", "Jude",
+
+            // Apocalyptic Literature
+            "Revelation"
+        );
+        assertEquals(books, canon.getBooks());
+    }
+}
