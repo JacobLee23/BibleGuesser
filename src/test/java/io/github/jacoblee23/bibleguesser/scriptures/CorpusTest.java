3ef89c597ca73dcf3215e69c8387a49f6004e3a6
Jacob Lee <Jacob.J.Lee@outlook.com>
2026-03-09 13:23:04 -0700

    Update bibleguesser.scriptures.Corpus to translate between citations and indices

--- src/test/java/io/github/jacoblee23/bibleguesser/scriptures/CorpusTest.java
+++ src/test/java/io/github/jacoblee23/bibleguesser/scriptures/CorpusTest.java
@@ -24,6 +24,17 @@ public class CorpusTest {
     // Total number of verses in New Testament of Biblical canon
     private static final int NVERSES_NT = 7957;
 
+    // Bookened verses of the Bible
+    private static final Citation OT_START = new Citation("Genesis", 1, 1);
+    private static final Citation OT_END = new Citation("Malachi", 4, 6);
+    private static final Citation NT_START = new Citation("Matthew", 1, 1);
+    private static final Citation NT_END = new Citation("Revelation", 22, 21);
+
+    // Midpoint verses of the Bible
+    private static final Citation MIDPOINT = new Citation("Psalms", 103, 1);
+    private static final Citation OT_MIDPOINT = new Citation("2 Chronicles", 18, 30);
+    private static final Citation NT_MIDPOINT = new Citation("Acts", 7, 7);
+
     @ParameterizedTest
     @EnumSource(Translations.Translation.class)
     void testGetTranslation(Translations.Translation translation) {
@@ -155,5 +166,173 @@ public class CorpusTest {
         } else {
             Assertions.assertEquals(CorpusTest.NVERSES_NT, lengthNT, translation.toString());
         }
+    }
+
+    @ParameterizedTest
+    @EnumSource(Translations.Translation.class)
+    void testGetIndex(Translations.Translation translation) {
+        Exception exception;
+
+        Corpus corpus = Corpus.getInstance(translation);
+
+        // Invalid chapters contained in citation
+        Citation invalidChapter = new Citation("Psalms", corpus.nchapters("Psalms") + 1, 1);
+        exception = Assertions.assertThrows(
+            IllegalArgumentException.class, () -> {
+                corpus.getIndex(invalidChapter);
+            }, translation.toString()
+        );
+        Assertions.assertEquals(
+            String.format(
+                "Invalid chapter number contained in citation: %s", invalidChapter
+            ), exception.getMessage(), translation.toString()
+        );
+
+        // Invalid verses contained in citation
+        Citation invalidVerse = new Citation("Psalms", 1, corpus.nverses("Psalms", 1) + 1);
+        exception = Assertions.assertThrows(
+            IllegalArgumentException.class, () -> {
+                corpus.getIndex(invalidVerse);
+            }, translation.toString()
+        );
+        Assertions.assertEquals(
+            String.format(
+                "Invalid verse number contained in citation: %s", invalidVerse
+            ), exception.getMessage(), translation.toString()
+        );
+
+        // Bookend verses of Bible
+        Assertions.assertEquals(
+            0, corpus.getIndex(CorpusTest.OT_START), translation.toString()
+        );
+        Assertions.assertEquals(
+            CorpusTest.NVERSES_OT - 1, corpus.getIndex(CorpusTest.OT_END), translation.toString()
+        );
+        Assertions.assertEquals(
+            CorpusTest.NVERSES_OT, corpus.getIndex(CorpusTest.NT_START), translation.toString()
+        );
+        if (
+            translation == Translations.Translation.AMPC
+            || translation == Translations.Translation.ESV
+        ) {
+            Assertions.assertEquals(
+                CorpusTest.NVERSES_TOTAL,
+                corpus.getIndex(CorpusTest.NT_END),
+                translation.toString()
+            );
+        } else {
+            Assertions.assertEquals(
+                CorpusTest.NVERSES_TOTAL - 1,
+                corpus.getIndex(CorpusTest.NT_END),
+                translation.toString()
+            );
+        }
+
+        // Midpoint verses of Bible
+        Assertions.assertEquals(
+            (CorpusTest.NVERSES_TOTAL + 1) / 2 - 1,
+            corpus.getIndex(CorpusTest.MIDPOINT),
+            translation.toString()
+        );
+        Assertions.assertEquals(
+            (CorpusTest.NVERSES_OT + 1) / 2 - 1,
+            corpus.getIndex(CorpusTest.OT_MIDPOINT),
+            translation.toString()
+        );
+        Assertions.assertEquals(
+            CorpusTest.NVERSES_OT + (CorpusTest.NVERSES_NT + 1) / 2 - 1,
+            corpus.getIndex(CorpusTest.NT_MIDPOINT),
+            translation.toString()
+        );
+    }
+
+    @ParameterizedTest
+    @EnumSource(Translations.Translation.class)
+    void testGetCitation(Translations.Translation translation) {
+        Exception exception;
+
+        Corpus corpus = Corpus.getInstance(translation);
+
+        // Lower bound of valid index range exceeded
+        exception = Assertions.assertThrows(
+            IllegalArgumentException.class, () -> {
+                corpus.getCitation(-1);
+            }, translation.toString()
+        );
+        Assertions.assertEquals(
+            "Invalid verse index: -1", exception.getMessage(), translation.toString()
+        );
+
+        // Upper bound of valid index range exceeded
+        if (
+            translation == Translations.Translation.AMPC
+            || translation == Translations.Translation.ESV
+        ) {
+            exception = Assertions.assertThrows(
+                IllegalArgumentException.class, () -> {
+                    corpus.getCitation(CorpusTest.NVERSES_TOTAL + 1);
+                }, translation.toString()
+            );
+            Assertions.assertEquals(
+                String.format(
+                    "Invalid verse index: %d", CorpusTest.NVERSES_TOTAL + 1
+                ), exception.getMessage(), translation.toString()
+            );
+        } else {
+            exception = Assertions.assertThrows(
+                IllegalArgumentException.class, () -> {
+                    corpus.getCitation(CorpusTest.NVERSES_TOTAL);
+                }, translation.toString()
+            );
+            Assertions.assertEquals(
+                String.format(
+                    "Invalid verse index: %d", CorpusTest.NVERSES_TOTAL
+                ), exception.getMessage(), translation.toString()
+            );
+        }
+
+        // Bookend verses of Bible
+        Assertions.assertEquals(
+            CorpusTest.OT_START, corpus.getCitation(0), translation.toString()
+        );
+        Assertions.assertEquals(
+            CorpusTest.OT_END, corpus.getCitation(CorpusTest.NVERSES_OT - 1), translation.toString()
+        );
+        Assertions.assertEquals(
+            CorpusTest.NT_START, corpus.getCitation(CorpusTest.NVERSES_OT), translation.toString()
+        );
+        if (
+            translation == Translations.Translation.AMPC
+            || translation == Translations.Translation.ESV
+        ) {
+            Assertions.assertEquals(
+                CorpusTest.NT_END,
+                corpus.getCitation(CorpusTest.NVERSES_TOTAL),
+                translation.toString()
+            );
+        } else {
+            Assertions.assertEquals(
+                CorpusTest.NT_END,
+                corpus.getCitation(CorpusTest.NVERSES_TOTAL - 1),
+                translation.toString()
+            );
+        }
+
+        // Midpoint verses of Bible
+        Assertions.assertEquals(
+            CorpusTest.MIDPOINT,
+            corpus.getCitation((CorpusTest.NVERSES_TOTAL + 1) / 2 - 1),
+            translation.toString()
+        );
+        Assertions.assertEquals(
+            CorpusTest.OT_MIDPOINT,
+            corpus.getCitation((CorpusTest.NVERSES_OT + 1) / 2 - 1),
+            translation.toString()
+        );
+        Assertions.assertEquals(
+            CorpusTest.NT_MIDPOINT,
+            corpus.getCitation(CorpusTest.NVERSES_OT + (CorpusTest.NVERSES_NT + 1) / 2 - 1),
+            translation.toString()
+        );
     }
 }
